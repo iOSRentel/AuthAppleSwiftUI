1d7409a3e89c462d5acfbda2ed9ea50a1029891f
iOSRentel <88737803+iOSRentel@users.noreply.github.com>
2021-08-28 14:15:25 +0300

    Initial Commit

---
+++ AuthAppleSwiftUI/AuthAppleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAppleSwiftUIApp.swift
+//  AuthAppleSwiftUI
+//
+//  Created by Boris Zinovyev on 28.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AuthAppleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthAppleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthAppleSwiftUI
+//
+//  Created by Boris Zinovyev on 28.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
